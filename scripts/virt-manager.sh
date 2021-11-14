5e59838bfce1acb4b284007ec17761db899c57bc
farook <farookphuket@gmail.com>
2021-11-14 18:05:37 +0700

    update virt manager install

--- scripts/virt-manager.sh
+++ scripts/virt-manager.sh
@@ -1,10 +1,14 @@
 #!/bin/bash 
 
 
+CONFIG_FILE_DIR=~/archlinux_my_config/CONFIG_FILES 
 sudo pacman -S dmidecode edk2-ovmf --needed
 
      
 sudo pacman -S --needed virt-manager qemu vde2 ebtables dnsmasq bridge-utils openbsd-netcat 
+
+
+sudo cp -r $CONFIG_FILE_DIR/networks /usr/share/libvirt/ 
 
 # enable libvirt on boot
 sudo systemctl enable libvirtd
