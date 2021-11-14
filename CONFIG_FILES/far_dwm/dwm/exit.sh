7203aa577819c1a6b1a270b8aebaf49a9b38cd66
farook <farookphuket@gmail.com>
2021-11-14 20:14:47 +0700

    update from arch linux desktop 13 nov 2021

--- CONFIG_FILES/far_dwm/dwm/exit.sh
+++ CONFIG_FILES/far_dwm/dwm/exit.sh
@@ -8,29 +8,29 @@ WIDTH=45
 CHOICE_WIDTH=4
 TITLE="Simple logout script"
 BACK_TITLE="$USER's simple logout script!"
-MENU="What would you like to do? : "
+MENU="What would you like to do? $USER: "
 
     OPTIONS=(1 "Reboot"
         2 "Shutdown Now"
         3 "Logout")
-
+        
         CHOICE=$(dialog --clear \
-                --backtitle "$BACK_TITLE" \
-                --title "$TITLE" \
-                --menu "$MENU" \
-                $HEIGHT $WIDTH $CHOICE_WIDTH \
-                "${OPTIONS[@]}" \
-                2>&1 >/dev/tty)
+            --title "$TITLE" \
+            --backtitle "$BACK_TITLE" \
+            --menu "$MENU" \
+            $HEIGHT $WIDTH $CHOICE_WIDTH  \
+            2>&1 >/dev/tty \
+            "${OPTIONS[@]}")
 
-                clear 
-                case $CHOICE in
-                        1) 
-                            reboot
-                            ;;
-                        2)
-                            shutdown now
-                            ;;
-                        3)
-                            killall dwm
-                            ;;
-                esac
+            clear
+            case $CHOICE in
+                1) 
+                    reboot
+                    ;;
+                2) 
+                    shutdown now
+                    ;;
+                3) 
+                    killall dwm
+                        ;;
+            esac
