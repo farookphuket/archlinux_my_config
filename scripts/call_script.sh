b5237889f1ff754ac0033d52f04e23780a15a9d1
farook <farookphuket@gmail.com>
2022-02-17 02:07:03 +0700

    added key for program search 16 Feb 2022

--- scripts/call_script.sh
+++ scripts/call_script.sh
@@ -18,13 +18,13 @@ sh $run_script_dir/code_editor.sh
 sh $run_script_dir/download_tools.sh 
 
 
-sh $run_script_dir/gimp.sh 
+#sh $run_script_dir/gimp.sh 
 
-sh $run_script_dir/gnome_app.sh 
+#sh $run_script_dir/gnome_app.sh 
 
 sh $run_script_dir/iso_image_writer.sh 
 
-sh $run_script_dir/kde_app.sh 
+#sh $run_script_dir/kde_app.sh 
 
 sh $run_script_dir/libre_office.sh 
 
@@ -32,13 +32,13 @@ sh $run_script_dir/media_player_app.sh
 
 sh $run_script_dir/screen_capture.sh
 
-sh $run_script_dir/shutter.sh 
+#sh $run_script_dir/shutter.sh 
 
-sh $run_script_dir/virt-manager.sh 
+#sh $run_script_dir/virt-manager.sh 
 
-sh $run_script_dir/virtualbox.sh 
+#sh $run_script_dir/virtualbox.sh 
 
-sh $run_script_dir/xfce4_app.sh 
+#sh $run_script_dir/xfce4_app.sh 
 
 sh $run_script_dir/applet.sh
 
@@ -51,4 +51,4 @@ sh $run_script_dir/other_app.sh
 sh $run_script_dir/web_browser.sh
 
 # install puddletag on 09 Jan 2022
-sh $run_script_dir/puddletag.sh
+#sh $run_script_dir/puddletag.sh
