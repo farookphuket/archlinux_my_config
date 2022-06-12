7dad8bf65305d768a68afb42f28a5f67fb78b272
farook <farookphuket@gmail.com>
2022-06-12 10:18:33 +0700

    update blowfish_secret for phpmyadmin 12-jun-2022

--- SETUP_LAMP_SERVER/files_inc/config.inc.php
+++ SETUP_LAMP_SERVER/files_inc/config.inc.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = '**dfjdfjdfjdk jsttdhsjd ^%3434 () *7eef *d'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'ThisabCchahaslopGdTYdUfggoipkgjt'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 
 $cfg['TempDir'] = '/tmp/phpmyadmin';
