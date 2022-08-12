5e0a7c905e48682e42195afff76176de3b27dde3
farook <farookphuket@gmail.com>
2022-08-12 19:13:50 +0700

    install vivaldi on 8 aug 2022

--- scripts/web_browser.sh
+++ scripts/web_browser.sh
@@ -4,4 +4,8 @@
 
 yay -S google-chrome brave-bin 
 
-sudo pacman -S firefox chromium 
+sudo pacman -S firefox chromium vivaldi 
+
+# install vivaldi on 8 aug 2022 
+# the shortcut key is Super+F5 
+# call in shortcut vivaldi-stable
