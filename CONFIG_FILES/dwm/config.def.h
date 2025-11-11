abbfdd1be370d4f84602aae98b194066906a6b90
Alfar farook <firefrook@gmail.com>
2025-11-12 03:14:52 +0700

    re-install conky 12 nov. 2025

--- CONFIG_FILES/dwm/config.def.h
+++ CONFIG_FILES/dwm/config.def.h
@@ -81,7 +81,9 @@ static Key keys[] = {
 
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+/* need to remove "t" to bind with `Super+t` to spawn `Thunar file manager`
+ * { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, 
+ * */
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
